c85fb7f97b2e2bc81a56e1d40433e4653a690a01
MichaelBarbuzano <michael.barbuzano@snhu.edu>
2023-10-27 04:44:05 -0400

    Complete Module 2

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const router = express.Router();
-const controller= require('../controllers/travel');
-/* GET home page. */
-router.get('/', controller.travel);
-module.exports = router;
+/* GET travel view */
+const travel = (req, res) => {
+    res.render('travel', { title: 'Travlr Getaways' });
+   };
+   module.exports = {
+    travel
+   };
