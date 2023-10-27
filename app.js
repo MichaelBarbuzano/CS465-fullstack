3f55dcb9f37619d6ad0780c7b37d3762c1ab6b82
MichaelBarbuzano <michael.barbuzano@snhu.edu>
2023-10-27 04:06:12 -0400

    Module 2

--- app.js
+++ app.js
@@ -4,14 +4,19 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./app_server/routes/index');
+var usersRouter = require('./app_server/routes/users');
+const travelRouter = require('./app_server/controllers/travel');
 
 var app = express();
 
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join('app_server', 'views'));
 app.set('view engine', 'hbs');
+
+//register handlebars and partials
+hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'));
 
 app.use(logger('dev'));
 app.use(express.json());
