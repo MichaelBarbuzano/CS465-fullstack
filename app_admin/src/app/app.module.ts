41d1dac92f08993f527acad7de5f7b7c5365157f
MichaelBarbuzano <michael.barbuzano@snhu.edu>
2023-12-10 15:16:11 -0500

    Up to step 25

--- app_admin/src/app/app.module.ts
+++ app_admin/src/app/app.module.ts
@@ -11,6 +11,8 @@ import { TripDataService } from './services/trip-data.service';
 import { AddTripComponent } from './add-trip/add-trip.component';
 import { EditTripComponent } from './edit-trip/edit-trip.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +21,9 @@ import { NavbarComponent } from './navbar/navbar.component';
     TripCardComponent,
     AddTripComponent,
     EditTripComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
